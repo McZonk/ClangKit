efcacef1a56175f909987d53a65a3934d1319db3
Jean-David Gadina <macmade@xs-labs.com>
2014-05-14 20:41:57 +0200

    Cleanup…

--- ClangKit/CKWarnings.h
+++ ClangKit/CKWarnings.h
@@ -153,7 +153,6 @@
 #pragma clang diagnostic fatal "-Wpointer-arith"
 #pragma clang diagnostic fatal "-Wpointer-to-int-cast"
 #pragma clang diagnostic fatal "-Wprotocol"
-#pragma clang diagnostic fatal "-Wreadonly-setter-attrs"
 #pragma clang diagnostic fatal "-Wredundant-decls"
 #pragma clang diagnostic fatal "-Wreorder"
 #pragma clang diagnostic fatal "-Wreturn-type"
@@ -208,6 +207,7 @@
 /*
  * Not recognized by Apple implementation:
  * 
+ * #pragma clang diagnostic fatal "-Wreadonly-setter-attrs"
  * #pragma clang diagnostic fatal "-Wdefault-arg-special-member"
  * #pragma clang diagnostic fatal "-Wauto-import"
  * #pragma clang diagnostic fatal "-Wbuiltin-requires-header"
