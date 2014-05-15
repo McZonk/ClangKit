5ae1a7cbdc2133f95bd2f9ee86ab6134f5dbf4fb
Jean-David Gadina <macmade@xs-labs.com>
2014-05-15 19:26:49 +0200

    Documentation in progress…

--- ClangKit/Classes/CKSourceLocation.h
+++ ClangKit/Classes/CKSourceLocation.h
@@ -43,12 +43,49 @@
     NSString   * _fileName;
 }
 
-@property( atomic, readonly ) void       * ptrData1;
-@property( atomic, readonly ) void       * ptrData2;
-@property( atomic, readonly ) unsigned int intData;
-@property( atomic, readonly ) NSString   * fileName;
+/*!
+ * @property        ptrData1
+ * @abstract        Internal pointer data 1
+ */
+@property( atomic, readonly ) void * ptrData1;
 
+/*!
+ * @property        ptrData2
+ * @abstract        Internal pointer data 1
+ */
+@property( atomic, readonly ) void * ptrData2;
+
+/*!
+ * @property        intData
+ * @abstract        Internal integer data
+ */
+@property( atomic, readonly ) unsigned int intData;
+
+/*!
+ * @property        fileName
+ * @abstract        The source location's filename
+ */
+@property( atomic, readonly ) NSString * fileName;
+
+/*!
+ * @method          sourceLocationWithPointerData1:pointerData2:intData:
+ * @abstract        Gets a source location object
+ * @param           ptrData1    Pointer data 1
+ * @param           ptrData2    Pointer data 2
+ * @param           intData     Integer data
+ * @return          The source location object
+ * @discussion      The returned object is autoreleased.
+ */
 + ( id )sourceLocationWithPointerData1: ( void * )ptrData1 pointerData2: ( void * )ptrData2 intData: ( unsigned int )intData;
+
+/*!
+ * @method          initWithPointerData1:pointerData2:intData:
+ * @abstract        Initializes a source location object
+ * @param           ptrData1    Pointer data 1
+ * @param           ptrData2    Pointer data 2
+ * @param           intData     Integer data
+ * @return          The source location object
+ */
 - ( id )initWithPointerData1: ( void * )ptrData1 pointerData2: ( void * )ptrData2 intData: ( unsigned int )intData;
 
 @end
--- ClangKit/Classes/CKTranslationUnit.h
+++ ClangKit/Classes/CKTranslationUnit.h
@@ -94,23 +94,179 @@
  */
 @property( atomic, readonly ) CXFile cxFile;
 
+/*!
+ * @method          translationUnitWithPath:
+ * @abstract        Gets a translation unit from a path
+ * @param           path        The path
+ * @return          The translation unit object
+ * @discussion      The returned object is autoreleased.
+ */
 + ( id )translationUnitWithPath: ( NSString * )path;
+
+/*!
+ * @method          translationUnitWithPath:index:
+ * @abstract        Gets a translation unit from a path and an index
+ * @param           path        The path
+ * @param           index       The index
+ * @return          The translation unit object
+ * @discussion      The returned object is autoreleased.
+ */
 + ( id )translationUnitWithPath: ( NSString * )path index: ( CKIndex * )index;
+
+/*!
+ * @method          translationUnitWithPath:args:
+ * @abstract        Gets a translation unit from a path and arguments
+ * @param           path        The path
+ * @param           args        The arguments
+ * @return          The translation unit object
+ * @discussion      The returned object is autoreleased.
+ */
 + ( id )translationUnitWithPath: ( NSString * )path args: ( NSArray * )args;
+
+/*!
+ * @method          translationUnitWithPath:index:args:
+ * @abstract        Gets a translation unit from a path, index and arguments
+ * @param           path        The path
+ * @param           index       The index
+ * @param           args        The arguments
+ * @return          The translation unit object
+ * @discussion      The returned object is autoreleased.
+ */
 + ( id )translationUnitWithPath: ( NSString * )path index: ( CKIndex * )index args: ( NSArray * )args;
+
+/*!
+ * @method          translationUnitWithText:language:
+ * @abstract        Gets a translation unit from a text
+ * @param           text        The text
+ * @param           language    The language to use
+ * @return          The translation unit object
+ * @discussion      The returned object is autoreleased.
+ */
 + ( id )translationUnitWithText: ( NSString * )text language: ( CKLanguage )language;
+
+/*!
+ * @method          translationUnitWithText:language:index:
+ * @abstract        Gets a translation unit from a text and an index
+ * @param           text        The text
+ * @param           language    The language to use
+ * @param           index       The index
+ * @return          The translation unit object
+ * @discussion      The returned object is autoreleased.
+ */
 + ( id )translationUnitWithText: ( NSString * )text language: ( CKLanguage )language index: ( CKIndex * )index;
+
+/*!
+ * @method          translationUnitWithText:language:args:
+ * @abstract        Gets a translation unit from a text and arguments
+ * @param           text        The text
+ * @param           language    The language to use
+ * @param           args        The arguments
+ * @return          The translation unit object
+ * @discussion      The returned object is autoreleased.
+ */
 + ( id )translationUnitWithText: ( NSString * )text language: ( CKLanguage )language args: ( NSArray * )args;
+
+/*!
+ * @method          translationUnitWithText:language:index:args:
+ * @abstract        Gets a translation unit from a text, index and arguments
+ * @param           text        The text
+ * @param           language    The language to use
+ * @param           index       The index
+ * @param           args        The arguments
+ * @return          The translation unit object
+ * @discussion      The returned object is autoreleased.
+ */
 + ( id )translationUnitWithText: ( NSString * )text language: ( CKLanguage )language index: ( CKIndex * )index args: ( NSArray * )args;
+
+/*!
+ * @method          initWithPath:
+ * @abstract        Initializes a translation unit with a path
+ * @param           path        The path
+ * @return          The translation unit objectb                                                                                                                                             
+ */
 - ( id )initWithPath: ( NSString * )path;
+
+/*!
+ * @method          initWithPath:index:
+ * @abstract        Initializes a translation unit from a path and an index
+ * @param           path        The path
+ * @param           index       The index
+ * @return          The translation unit object
+ */
 - ( id )initWithPath: ( NSString * )path index: ( CKIndex * )index;
+
+/*!
+ * @method          initWithPath:args:
+ * @abstract        Initializes a translation unit from a path and arguments
+ * @param           path        The path
+ * @param           args        The arguments
+ * @return          The translation unit object
+ */
 - ( id )initWithPath: ( NSString * )path args: ( NSArray * )args;
+
+/*!
+ * @method          initWithPath:index:args:
+ * @abstract        Initializes a translation unit from a path, index and arguments
+ * @param           path        The path
+ * @param           index       The index
+ * @param           args        The arguments
+ * @return          The translation unit object
+ */
 - ( id )initWithPath: ( NSString * )path index: ( CKIndex * )index args: ( NSArray * )args;
+
+/*!
+ * @method          initWithText:language:
+ * @abstract        Initializes a translation unit from a text
+ * @param           text        The text
+ * @param           language    The language to use
+ * @return          The translation unit object
+ */
 - ( id )initWithText: ( NSString * )text language: ( CKLanguage )language;
+
+/*!
+ * @method          initWithText:language:index:
+ * @abstract        Initializes a translation unit from a text and an index
+ * @param           text        The text
+ * @param           language    The language to use
+ * @param           index       The index
+ * @return          The translation unit object
+ */
 - ( id )initWithText: ( NSString * )text language: ( CKLanguage )language index: ( CKIndex * )index;
+
+/*!
+ * @method          initWithText:language:args:
+ * @abstract        Initializes a translation unit from a text and arguments
+ * @param           text        The text
+ * @param           language    The language to use
+ * @param           args        The arguments
+ * @return          The translation unit object
+ */
 - ( id )initWithText: ( NSString * )text language: ( CKLanguage )language args: ( NSArray * )args;
+
+/*!
+ * @method          initWithText:language:index:args:
+ * @abstract        Initializes a translation unit from a text, index and arguments
+ * @param           text        The text
+ * @param           language    The language to use
+ * @param           index       The index
+ * @param           args        The arguments
+ * @return          The translation unit object
+ */
 - ( id )initWithText: ( NSString * )text language: ( CKLanguage )language index: ( CKIndex * )index args: ( NSArray * )args;
+
+/*!
+ * @method          reparse
+ * @abstract        Reparses the translation unit
+ */
 - ( void )reparse;
+
+/*!
+ * @method          completionResultsForLine:column:
+ * @abstract        Gets completion results for a specific line and column
+ * @param           line        The line number
+ * @param           column      The column number
+ * @return          An array of completion results
+ */
 - ( NSArray * )completionResultsForLine: ( NSUInteger )line column: ( NSUInteger )column;
 
 @end
