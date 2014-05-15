2f681893536966cf6a846bf563f50bcf2abdb0f2
Jean-David Gadina <macmade@xs-labs.com>
2014-05-15 20:51:29 +0200

    Documentation update...

--- Documentation/index.php
+++ Documentation/index.php
@@ -41,6 +41,39 @@ $DOC->addPage( 'Pages/Tutorial.inc.php', 'Tutorial' );
     <meta name="rating" content="General" />
     <meta name="robots" content="all" />
     <meta name="generator" content="XSDocgen" />
+    <script type="text/javascript">
+        // <![CDATA[
+        
+        (
+            function( i, s, o, g, r, a, m )
+            {
+                i[ 'GoogleAnalyticsObject' ] = r;
+                i[ r ]                       = i[ r ] || function()
+                {
+                    ( i[ r ].q = i[ r ].q || [] ).push( arguments )
+                },
+                i[ r ].l = 1 * new Date();
+                a        = s.createElement( o ),
+                m        = s.getElementsByTagName( o )[ 0 ];
+                a.async  = 1;
+                a.src    = g;
+            
+                m.parentNode.insertBefore( a, m )
+            }
+        )
+        (
+            window,
+            document,
+            'script',
+            '//www.google-analytics.com/analytics.js',
+            'ga'
+        );
+        
+        ga( 'create', 'UA-51035898-4', 'xs-labs.com' );
+        ga( 'send', 'pageview' );
+        
+        // ]]>
+    </script>
 </head>
 <body>
 <?php
